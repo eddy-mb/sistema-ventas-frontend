d994556aec26cfa2e689ae6ec0ca81631ddb9f16
Eddy Mamani <eddy.mamani@agetic.gob.bo>
2025-03-22 18:09:26 -0400

    definiendo estructura base para el proyecto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Hola mundo</div>;
+  return (
+    <div>
+      <Button variant="destructive">Hola mundo</Button>
+    </div>
+  );
 }
