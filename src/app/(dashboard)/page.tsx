ca0c6fd63f7c1bcd1dce23c280a9d8747eaf3d12
Eddy Mamani <eddy.mamani@agetic.gob.bo>
2025-03-24 01:53:16 -0400

    feat: se actualiza el layout del dashboard con componentes de shadcn

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+import { useEffect } from "react";
+import { toast } from "sonner";
+
 export default function Page() {
+  useEffect(() => {
+    toast.success("Prueba de Toast");
+    console.log("Hola");
+  }, []);
+
   return <div>Pagina de Dashboard</div>;
 }
